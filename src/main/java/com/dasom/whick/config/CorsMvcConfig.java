6684a5b2c7cf55ead406955ee005e174a311ecdc
HuitaePark <qkrgmlxo3174@gmail.com>
2024-10-14 14:58:44 +0900

    cors 설정 추가

---
+++ src/main/java/com/dasom/whick/config/CorsMvcConfig.java
@@ -0,0 +1,17 @@
+package com.dasom.whick.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
---
+++ src/main/java/com/dasom/whick/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.dasom.whick.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        return configuration;
+                    }
+                })));
+                // CSRF 보호 비활성화 (API 서버의 경우 일반적으로 비활성화)
+        http
+                .csrf((auth) -> auth.disable());
+        http
+                // 권한 설정
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                ); // 그 외의 요청은 인증 필요
+
+        return http.build();
+    }
+}
